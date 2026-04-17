50493669b53adcce13b0ea1daab2de42f6d49e8c
vorillaz <theodorevorillas@gmail.com>
2026-04-17 11:35:57 +0300

    chore(codegen): split generators into colorful + mono component output

--- packages/codegen/src/generators/react.ts
+++ packages/codegen/src/generators/react.ts
@@ -1,55 +1,30 @@
-import { normalizeProps } from '@dev.icons/utils';
-import type { Generator, IconRecord } from '../types';
+import { normalizeProps } from "@dev.icons/utils";
+import type { Generator, IconRecord, Variant } from "../types";
 
-const defsTemplate = (icon: IconRecord) => {
-  const iconBody = normalizeProps(icon.iconSvg);
-  const fontBody = normalizeProps(icon.fontSvg);
+const componentTemplate = (icon: IconRecord, variant: Variant) => {
+  const body = normalizeProps(variant === "icon" ? icon.iconSvg : icon.fontSvg);
   return `/* GENERATED FILE - DO NOT EDIT */
-import React, { ReactElement } from "react";
-import { IconType } from "../lib";
-
-export default new Map<IconType, ReactElement>([
-  [
-    "icon",
-    <>
-      ${iconBody}
-    </>,
-  ],
-  [
-    "font",
-    <>
-      ${fontBody}
-    </>,
-  ],
-]);
-`;
-};
-
-const componentTemplate = (icon: IconRecord) =>
-  `/* GENERATED FILE - DO NOT EDIT */
 import React, { forwardRef } from "react";
 import type { Icon } from "../lib/types";
 import SSRBase from "../lib/ssr";
-import icons from "../defs/${icon.componentName}";
 
 const I: Icon = forwardRef((props, ref) => (
-  <SSRBase ref={ref} {...props} icons={icons} />
+  <SSRBase ref={ref} {...props}>
+    ${body}
+  </SSRBase>
 ));
 
 I.displayName = "${icon.componentName}";
 export { I as ${icon.componentName} };
 `;
+};
 
 export const reactGenerator: Generator = {
-  extension: 'tsx',
-  defsExtension: 'tsx',
-  resolveName: base => base,
-  emit: icon => ({
-    defs: defsTemplate(icon),
-    component: componentTemplate(icon),
-  }),
-  emitBarrel: names =>
+  extension: "tsx",
+  resolveName: (base) => base,
+  emitComponent: componentTemplate,
+  emitBarrel: (names) =>
     `/* GENERATED FILE - DO NOT EDIT */
-${names.map(n => `export * from "./${n}";`).join('\n')}
+${names.map((n) => `export * from "./${n}";`).join("\n")}
 `,
 };
